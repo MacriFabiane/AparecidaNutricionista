5361d2a02475e6cbbfff36c8e0fc47b0d1a794ef
MacriFabiane <mariacristinafabiane@gmail.com>
2024-02-15 14:58:15 -0300

    creating

---
+++ js/principal.js
@@ -0,0 +1,6 @@
+//console.log("Olá, Mundo!");
+//console.log(document);
+var titulo = document.querySelector(".titulo"); //salvando Ap Nutrição numa var
+//console.log(titulo);
+//textContent acessa somente o texto sem a tag
+titulo.textContent= "Aparecida Nutricionista"; //mudando o titulo com js
