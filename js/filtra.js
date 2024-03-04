58204d0a2f8699bbe04d0c5794f1961f5475ac2b
MacriFabiane <mariacristinafabiane@gmail.com>
2024-03-04 09:54:51 -0300

    arrumando a filtragem

--- js/filtra.js
+++ js/filtra.js
@@ -4,8 +4,25 @@ campoFiltro.addEventListener("input", function(){
     console.log(this.value);
     var pacientes= document.querySelectorAll(".paciente");
 
-    pacientes.forEach(function(paciente){
-        var tdNome = paciente.querySelector(".info-nome");
-        var nome = tdNome.textContent;
-    });
+    if(this.value.length >0){
+        for(var i=0; i< pacientes.length; i++){
+            var paciente =pacientes[i];
+            var tdNome = paciente.querySelector(".info-nome");
+            var nome = tdNome.textContent;
+            var expressao =new RegExp(this.value, "i");//i significa q é case insensitive
+
+            if(!expressao.test(nome)){//ve se a expressao não contem nada em nome
+                paciente.classList.add("invisivel");
+            }
+            else{
+                paciente.classList.remove("invisivel");
+            }
+        }
+    }
+    else{
+        for(var i=0 ; i<pacientes.length; i++){
+            var paciente =pacientes[i];
+            paciente.classList.remove("invisivel");
+        }
+    }
 });
